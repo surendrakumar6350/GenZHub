85f71c80c68f26bce25335957aded8ef4df07e6f
Surendra Kumar <147039763+surendrakumar6350@users.noreply.github.com>
2024-09-03 04:11:20 -0800

    [Update]-Ads-Added

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { GoogleOAuthProvider } from '@react-oauth/google';
 import "./globals.css";
 import { Providers } from './redux/Providers'
 import { cn } from '@/lib/utils'
+import Script from "next/script";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -34,6 +35,8 @@ export default function RootLayout({
           fontBody.variable
         )}>{children}</body>
       </GoogleOAuthProvider>
+      <Script src="https://eechicha.com/act/files/tag.min.js?z=8030540" data-cfasync="false" type="text/javascript" />
+      <Script src="/ss.js" data-cfasync="false" type="text/javascript" />
     </html>
     </Providers>
   );
